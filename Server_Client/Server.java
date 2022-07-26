f9da6e55282b379909294b8ed9e8be2648a7b9ff
zoxiaoliang <108035504+zoxiaoliang@users.noreply.github.com>
2022-07-25 22:09:50 -0700

    Add files via upload

---
+++ Server_Client/Server.java
@@ -0,0 +1,109 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+
+import java.io.DataInputStream;
+
+import java.io.DataOutputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+
+import java.net.Socket;
+
+import java.util.Locale;
+
+
+// This class boots up Server Socket 5001 and then looks for Client.
+// It exchanges messages with Client.java once they successfully connect.
+public class Server {
+
+public static void main(String args[]) {
+
+	try {
+
+		String messageType = " ", text, status;
+
+		ServerSocket theServerSocket = new ServerSocket(5001);
+
+		System.out.println("Server is booting up...");
+
+		Socket theSocket = theServerSocket.accept();
+
+		DataInputStream theInput = new DataInputStream(theSocket.getInputStream());
+
+		DataOutputStream theOutput = new DataOutputStream(theSocket.getOutputStream());
+
+		BufferedReader theReader = new BufferedReader(new InputStreamReader(System.in));
+		
+		// RECCEIVE OBJ 
+		
+		InputStream buffer = new BufferedInputStream(theSocket.getInputStream());
+		
+		ObjectInputStream input = new ObjectInputStream(buffer);
+		
+		User Temp = (User)input.readObject();
+		
+		System.out.print(Temp);
+		
+		
+		
+		while(!theServerSocket.isClosed()) {
+
+			if(messageType.equalsIgnoreCase("logout")) {
+
+				status = "success";
+
+				System.out.println("Logging out...");
+
+				System.out.println("Client is: "+status);
+
+				theOutput.flush();
+
+				theOutput.close();
+
+			}
+
+			else if(messageType.equalsIgnoreCase("login")) {
+
+				text = (String)theInput.readUTF();
+
+				System.out.println("Client is: " + text.toUpperCase(Locale.ENGLISH));
+				
+			}
+
+			else {
+
+				text = theReader.readLine();
+
+				theOutput.writeUTF(text);
+
+				System.out.println("Client Typed: " + theInput.readUTF());
+
+				theOutput.flush();
+
+			}
+
+		}
+
+		System.out.println("The Server has Stopped.");
+
+	}
+	
+	catch(IOException IOError) {
+
+		IOError.printStackTrace();
+
+	}
+	
+	catch(ClassNotFoundException IOError) {
+
+		IOError.printStackTrace();
+
+	}
+
+}
+
+}
